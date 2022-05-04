d3c76c2f609942984764fd79edc3bcbb7c4250f9
ryanallenk <ryanallenk@gmail.com>
2022-05-03 22:03:20 -0400

    add exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+total_revenue = Store.all.sum(:annual_revenue)
+puts "Total revenue: #{total_revenue}"
+store_count = Store.count
+
+puts "Average revenue per store: #{total_revenue / store_count}"
+
+stores_over_1M = Store.where('annual_revenue >= 1000000')
+puts "Number of stores over 1M annually: #{stores_over_1M.count}"
